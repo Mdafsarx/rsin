05da3eaae63b17242418c3c84aa262a290e4b3fc
Mdafsarx <mdafsar99009@gmail.com>
2024-12-18 23:41:34 +0600

    footer 50% done

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,121 @@
+export default function Footer() {
+    return (
+        <div className="bg-white">
+            <div className="max-w-[82rem] mx-auto">
+                {/* top part */}
+                <footer className="footer text-base-content py-10">
+                    <div>
+                        <div><svg width="95" height="38" viewBox="0 0 153 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M110.549 36.9138V0.256348H118.854L144.797 26.6087V0.256348H153V36.9138H144.694L118.752 10.5102V36.9138H110.549Z" fill="#005397" />
+                            <path d="M95.1787 36.9138V0.256348H103.382V36.9138H95.1787Z" fill="#36B0BE" />
+                            <path d="M47.686 37.1701V30.5052L77.1658 30.5564C78.6697 30.5564 79.8147 30.2146 80.6008 29.531C81.4211 28.8133 81.8313 27.6683 81.8313 26.096C81.8313 24.5238 81.4211 23.3958 80.6008 22.7122C79.8147 21.9945 78.6697 21.6356 77.1658 21.6356H58.8114C55.0175 21.6356 52.061 20.7298 49.9419 18.9183C47.8569 17.0726 46.8145 14.3725 46.8145 10.8178C46.8145 7.26313 47.8569 4.58005 49.9419 2.76854C52.061 0.922846 55.0175 0 58.8114 0H87.6247V6.66499L59.683 6.61372C58.2133 6.61372 57.0683 6.95552 56.248 7.6391C55.4277 8.28851 55.0175 9.34808 55.0175 10.8178C55.0175 12.2875 55.4277 13.3642 56.248 14.0478C57.0683 14.6972 58.2133 15.0219 59.683 15.0219H78.0374C81.8313 15.0219 84.7707 15.9618 86.8557 17.8417C88.9748 19.6874 90.0344 22.4388 90.0344 26.096C90.0344 29.719 88.9748 32.4705 86.8557 34.3503C84.7707 36.2302 81.8313 37.1701 78.0374 37.1701H47.686Z" fill="#005397" />
+                            <path d="M0 36.9138V0.256348H30.7102C34.5041 0.256348 37.4436 1.21337 39.5285 3.12742C41.6477 5.04147 42.7072 7.94672 42.7072 11.8432C42.7072 15.1586 41.9382 17.7562 40.4001 19.6361C38.862 21.516 36.6745 22.6952 33.8376 23.1737L42.2971 36.9138H32.9661L25.1219 23.43H8.20307V36.9138H0ZM29.7361 6.92134H8.20307V16.7138L29.7361 16.765C31.24 16.765 32.385 16.389 33.1711 15.6371C33.9915 14.8852 34.4016 13.6205 34.4016 11.8432C34.4016 10.0317 33.9915 8.76703 33.1711 8.04926C32.385 7.29731 31.24 6.92134 29.7361 6.92134Z" fill="#005397" />
+                        </svg></div>
+                        <p className="max-w-sm leading-normal">Disclaimer: Due to the periodic changes of information/ requirement/ document, RSIN doesn’t provide any confirmation, guarantee or representation, express or implied, that the information contained or referenced herein is completely accurate or final. click here</p>
+                    </div>
+                    <nav>
+                        <h6 className="footer-title">Company</h6>
+                        <a className="link link-hover">About us</a>
+                        <a className="link link-hover">Contact</a>
+                        <a className="link link-hover">Jobs</a>
+                        <a className="link link-hover">Press kit</a>
+                    </nav>
+                    <nav>
+                        <h6 className="footer-title">Legal</h6>
+                        <a className="link link-hover">Terms of use</a>
+                        <a className="link link-hover">Privacy policy</a>
+                        <a className="link link-hover">Cookie policy</a>
+                    </nav>
+                    <nav>
+                        <h6 className="footer-title">Legal</h6>
+                        <a className="link link-hover">Terms of use</a>
+                        <a className="link link-hover">Privacy policy</a>
+                        <a className="link link-hover">Cookie policy</a>
+                    </nav>
+                    <div>
+                        <h6 className="footer-title">Action</h6>
+                        <button className="btn text-white rounded-full font-normal bg-[#005397] w-32">Start Investing</button>
+                        <button className="btn text-black rounded-full font-medium btn-outline border-[#005397] w-32 mt-2">Get Funded</button>
+                    </div>
+                </footer>
+                {/* bottom part */}
+                <footer className="footer text-base-content border-base-300 border-t py-4">
+                    <aside className="grid-flow-col items-center">
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            className="fill-current">
+                            <path
+                                d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+                        </svg>
+                        <p>
+                            ACME Industries Ltd.
+                            <br />
+                            Providing reliable tech since 1992
+                        </p>
+                    </aside>
+                    <nav className="md:place-self-center md:justify-self-end">
+                        <div className="grid grid-flow-col gap-4">
+                            <a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    className="fill-current">
+                                    <path
+                                        d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                                </svg>
+                            </a>
+                            <a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    className="fill-current">
+                                    <path
+                                        d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                                </svg>
+                            </a>
+                            <a>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    className="fill-current">
+                                    <path
+                                        d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </nav>
+                </footer>
+            </div>
+            {/* copy write part */}
+            <footer className="bg-[#F3F7FB]">
+                <div className="w-full mx-auto max-w-[82rem] py-4 md:flex md:items-center md:justify-between">
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                        Copyright RSIN@2024 . All rights reserved. V-0.1.14
+                    </span>
+                    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-800  sm:mt-0">
+                        <li>
+                            <a className="hover:underline me-4 md:me-6">Terms and Conditions</a>
+                        </li>
+                        <li>
+                            <a className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a className="hover:underline me-4 md:me-6">Cookie Settings</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
+        </div>
+    )
+}
